ff3a4994d7e73b7ff048aa897d87f8977d1dc7ab
Alpha <59319925+alphafurkan@users.noreply.github.com>
2021-03-26 12:15:11 +0300

    Update alpha.sql

--- alpha.sql
+++ alpha.sql
@@ -2,3 +2,5 @@ USE `es_extended`;
 
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
 	('armor', 'Çelik Yelek', 3, 0, 1);
+
+ALTER TABLE `users` ADD `armor` TINYINT UNSIGNED NOT NULL DEFAULT '0';
