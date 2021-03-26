3255a5b732a0119e214bb8004a0ae95f496d1416
Alpha <59319925+alphafurkan@users.noreply.github.com>
2021-03-26 12:14:32 +0300

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -24,3 +24,36 @@ AddEventHandler("alpha:removeArmor", function()
     xPlayer.removeInventoryItem("armor", 1)
 
 end)
+
+AddEventHandler('esx:playerLoaded', function(playerId)
+
+    local xPlayer = ESX.GetPlayerFromId(playerId)
+
+    MySQL.Async.fetchScalar("SELECT armor FROM users WHERE identifier = @identifier", { 
+        ['@identifier'] = xPlayer.getIdentifier()
+        }, function(data)
+        if (data ~= nil) then
+
+            ESX.SetTimeout(15000, function()
+		
+                TriggerClientEvent("alpha:updateArmor", playerId, data)
+            
+            end)
+
+        end
+    end)
+
+end)
+
+RegisterServerEvent('alpha:armorDB')
+AddEventHandler('alpha:armorDB', function(updateArmor)
+
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+
+    MySQL.Async.execute("UPDATE users SET armor = @armor WHERE identifier = @identifier", { 
+        ['@identifier'] = xPlayer.getIdentifier(),
+        ['@armor'] = tonumber(updateArmor)
+    })
+
+end)
